c18519f5ad7da1b0565cafe1b9b6c8acdfac46fd
nattharikanan <natthariknan@gmail.com>
2021-02-23 10:13:41 +0700

    abouUs

--- hellotest.py
+++ hellotest.py
@@ -5,6 +5,10 @@ app = Flask (__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template("aboutUs.html")
+
 @app.route('/Welcome/<name>')
 def Welcome_name(name):
     return 'Welcome' + ' ' + name + '!'
