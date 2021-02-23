175ffd30eb35227fafee862adf7cff9b6c8214c6
nattharikanan <natthariknan@gmail.com>
2021-02-23 10:08:29 +0700

    Flask

--- hellotest.py
+++ hellotest.py
@@ -1,1 +1,13 @@
-print("Hello Python")
+from flask import Flask, render_template
+app = Flask (__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/Welcome/<name>')
+def Welcome_name(name):
+    return 'Welcome' + ' ' + name + '!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
