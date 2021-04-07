79fff0f80173c25e414bf82bd0a0e6a8dca90293
nattharikanan <natthariknan@gmail.com>
2021-04-07 15:20:38 +0700

    test

--- hellotest.py
+++ hellotest.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template
 server = Flask (__name__)
 
-@app.route('/')
+@server.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/aboutUs')
-def aboutUs():
-    return render_template("aboutUs.html")
-
-@app.route('/Welcome/<name>')
-def Welcome_name(name):
-    return 'Welcome' + ' ' + name + '!'
-
 if __name__ == '__main__':
-     server.run(host = '0.0.0.0',port = 80)
+     server.run(host = '0.0.0.0',port =80)
